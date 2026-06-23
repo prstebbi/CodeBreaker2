dadee6eacc208214d0199dc6edb5c41e9291333a
prstebbi <prstebbins@outlook.com>
2026-06-22 20:19:13 -0600

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Paul Stebbins on 2026-06-22.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Paul Stebbins on 2026-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            Circle()
+            Circle()
+            Circle()
+            Circle()
+        }
+        
+    }
+    
+    
+}
+
+
+#Preview {
+    ContentView()
+}
