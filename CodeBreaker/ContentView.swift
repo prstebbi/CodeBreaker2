e47abf7b2e12bcca49b9370646efc51a13101c7c
prstebbi <prstebbins@outlook.com>
2026-07-13 16:40:06 -0600

    added ForEach

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -9,19 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            Circle()
-            Circle()
-            Circle()
-            Circle()
+        VStack {
+            pegs(colors: [.red, .green, .green, .yellow])
+            pegs(colors: [.red, .blue, .green, .red])
+            pegs(colors: [.red, .yellow, .green, .blue])
         }
-        
+        .padding()
     }
     
     
+    func pegs(colors: Array<Color>) -> some View {
+        HStack{
+            ForEach(colors.indices, id: \.self) { index in Circle().foregroundStyle(colors[index])
+            }
+        }
+    }
 }
+    
+    
 
 
 #Preview {
     ContentView()
 }
+
